a41504e5735b134c753bc6ec56dd8fcd9f8179ef
yubi00 <ubrajkhadka@gmail.com>
2020-07-09 18:16:20 +1000

    client folder modified

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import AppNavBar from './components/AppNavBar'
+import ShoppingList from './components/ShoppingList'
+import ItemModal from './components/ItemModal'
+import { Container } from 'reactstrap'
+import { Provider } from 'react-redux'
+import store from './store'
+
+import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <Provider store={store}>
+    <div className="App">
+      <AppNavBar/>
+      <Container>
+        <ItemModal/>
+        <ShoppingList/> 
+      </Container>
+      
+    </div>
+    </Provider>
+  );
+}
+
+export default App;
